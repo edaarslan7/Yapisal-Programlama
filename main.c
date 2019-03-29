a657d221620f071a15f1368ab6e7740bbb466a8a
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-03-29 15:35:14 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+/// Klavyeden girilen basari notuna göre ögrencinin harf notu karsiligini yazdiran bir program
+int main()
+{
+    int a;
+    printf("bir not giriniz: ");
+    scanf("%d",&a);
+    switch(a/10)
+    {
+        case 10:
+        case 9:
+            printf("AA");
+            break;
+        case 8:
+            if(a<85)
+            {
+                printf("BB");
+                break;
+            }
+            else
+            {
+                printf("BA");
+            }
+        case 7:
+            printf("CB");
+            break;
+        case 6:
+            printf("CC");
+            break;
+        case 5:
+            if(a<55)
+            {
+                printf("DD");
+                break;
+            }
+            else
+            {
+                printf("DC");
+                break;
+            }
+        case 4:
+            printf("FD");
+            break;
+        case 3:
+        case 2:
+        case 1:
+            printf("FF");
+            break;
+        default:
+            printf("1 ile 100 arasi deger giriniz...");
+            break;
+
+    }
+
+    return 0;
+}
