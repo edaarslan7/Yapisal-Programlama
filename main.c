ed3bc319f24136973b8e5bce02d4814219bb7395
Eda Arslan <34687342+edaarslan7@users.noreply.github.com>
2019-03-29 15:41:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+///Kullanýcýdan girilen sayinin birler ve onlar basamaginin toplami çift mi tek mi. sayi 4 basamakli olmali.
+int main()
+{
+    int sayi;
+    printf("bir sayi giriniz: ");
+    scanf("%d",&sayi);
+    if(sayi<999 || sayi>10000)
+    {
+        printf("hatali sayi girdiniz...");
+    }
+    else
+    {
+        int birler=sayi%10;
+        int onlar=((sayi-birler)/10)%10;
+        int toplam=birler+onlar;
+        if(toplam%2==0)
+        {
+            printf("toplam: %d cifttir...",toplam);
+        }
+        else
+        {
+            printf("toplam %d tektir...",toplam);
+        }
+    }
+    return 0;
+}
